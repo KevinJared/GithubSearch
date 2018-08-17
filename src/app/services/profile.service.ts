e21cf42ae076ca2fac54a3e652613b9f37427516
Kevin Jared <kevinjaredkip@gmail.com>
2018-08-17 16:47:36 +0300

    modified profile services

---
+++ src/app/services/profile.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import {Http, Headers} from '@angular/http';
+import 'rxjs/add/operator/map';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProfileService {
+
+  private username: string;
+  private clientid = '18445332';
+  private clientsecret = 'e10249fa54fb5ef332665d74db12d708ffe45281';
+
+  constructor(private http: Http) {
+    console.log('service is now ready');
+    this.username = 'KevinJared';
+  }
+
+    getProfileInfo() {
+      // tslint:disable-next-line:max-line-length
+      return this.http.get('https://api.github.com/users/' + this.username + '?client_id=' + this.clientid + '&client_secret=' + this.clientsecret)
+    .map(res => res.json());
+    }
+}
